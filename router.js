c0814a61d29b9233c6647b49b30f9ad653692d1b
NiteshBisht0075 <Nitesh.bisht@intelligaia.com>
2021-06-04 16:44:21 +0530

    mongodb Api

--- router.js
+++ router.js
@@ -1,19 +1,19 @@
 const koa = require('koa')
-const router =require('koa-router')
+const Router =require('koa-router')
 const koaBody =require('koa-body')
 require('./confi/DataBase')
 const controller = require('./controller/DataController')
-const Router = require('koa-router')
+// const Router = require('koa-router')
 const app = new koa();
 const router = new Router();
-const port = 9000;
+const port = 8000;
 
 router.post('/data-add',controller.data)
 module.exports = router.routes()
 
 app.use(koaBody({multipart:true}))
-.use(router.routes())
-.use(router.allowedMethods())
-.listen(port,() =>{
+app.use(router.routes())
+.use(router.allowedMethods());
+app.listen(port,() =>{
 console.log(`${port}`)
 })
